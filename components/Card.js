b69e8f7bb17107ae92463ec999a6ccf7be6e79ad
eko-develops <ekodevelops@gmail.com>
2021-09-11 02:37:25 -0700

    Finish cardlist and card layout

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,38 @@
+import Image from 'next/image'
+
 function Card({pokemon}) {
 
     const { abilities, location_area_encounters, moves, name, sprites, stats, types, order} = pokemon
-    console.log(pokemon)
 
     return (
         <div>
-            <h3>{name}</h3>
-            <p>Order: {order}</p>
+            <Image width={100} height={100} src={sprites.front_default} />
+            <h3>{name} #{order}</h3>
+            <p>Type(s): {types.map( (theType) => <span>{theType.type.name} </span> )}</p>
+            <div>
+                <p>Stats:</p>
+                <div>
+                    {
+                        stats.map( (stat) =>  (
+                            <span>
+                                {stat.stat.name} {stat.base_stat}
+                            </span>
+                        ))
+                    }
+                </div>
+            </div>
+            <div>
+                <p>Abilities:</p>
+                <div>
+                    {
+                        abilities.map( (ability) => (
+                            <span>
+                                {ability.ability.name + " "} 
+                            </span>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
