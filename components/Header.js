66e38d21534fbf5615f503be019b1f3e7c396b60
eko-develops <ekodevelops@gmail.com>
2021-09-10 23:43:28 -0700

    Layout header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,32 @@
+import Link from 'next/link'
+import headerStyles from '../styles/Header.module.css'
+
+
 const Header = () => {
     return (
-        <header>
-            this is the header
+        <header className={headerStyles.wrapper}>
+
+            <span className={headerStyles.brand}>Nextjs Poke</span>
+
+            <nav className={headerStyles.nav}>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/">
+                            <a>Search</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/">
+                            <a>Favourites</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,17 @@
 import Footer from "./Footer"
 import Header from "./Header"
+import layoutStyles from '../styles/Layout.module.css'
 
 function Layout( { children } ) {
     return (
-        <>
+        <div className={layoutStyles.container}>
             <Header />
                 <main>
                     {children}
                 </main>
             <Footer />
            
-        </>
+        </div>
     )
 }
 
