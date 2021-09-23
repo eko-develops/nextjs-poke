a5ab1f19e619811f8f5f4f62fd30cfb9758b0db8
eko-develops <ekodevelops@gmail.com>
2021-09-23 13:28:00 -0700

    Clean up index.js

--- pages/index.js
+++ pages/index.js
@@ -21,25 +21,24 @@ export default function Home( {pokemon} ) {
 
 export async function getStaticProps(context){
 
-    
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`);
     const data = await response.json();
     const results = data.results;
 
     const IMAGE_API_URL = `https://assets.pokemon.com/assets/cms2/img/pokedex/full`;
     const pokemon = results.map ( (pokemon, index) => {
-      const paddedIndex = ('00' + (index + 1)).slice(-3);
-      const imageUrl = `${IMAGE_API_URL}/${paddedIndex}.png`;
+        const paddedIndex = ('00' + (index + 1)).slice(-3);
+        const imageUrl = `${IMAGE_API_URL}/${paddedIndex}.png`;
 
-      return{
-        ...pokemon,
-        image: imageUrl
-      }
-    })
+         return{
+            ...pokemon,
+           image: imageUrl,
+        }
+       });
 
   return {
     props: {
-      pokemon
+      pokemon,
     }
   }
 }
